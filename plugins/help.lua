0a4ad184b731d43e5594c677107fddac44e26b5a
Arian <AllwenWaker81@gmail.com>
2016-08-21 01:08:16 -0700

    Update help.lua

--- plugins/help.lua
+++ plugins/help.lua
@@ -53,8 +53,6 @@ local function do_keyboard_private()
     keyboard.inline_keyboard = {
     	{
     		{text = '👥 Add me to a group', url = 'https://telegram.me/'..bot.username..'?startgroup=new'},
-    	    },
-    	    {
     		{text = '🎫 Channel', url = 'https://telegram.me/'..config.channel:gsub('@', '')},
 	    },
 	    {
