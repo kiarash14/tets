02af7b1ec146295c56ca8e21312759e741f61f82
Arian <AllwenWaker81@gmail.com>
2016-08-22 07:35:34 -0700

    Update starting.lua

--- plugins/starting.lua
+++ plugins/starting.lua
@@ -43,7 +43,7 @@ local function make_keyboard(mod, mod_current_position)
  else
      bottom_bar = {{text = '👤 ادمین ها', callback_data = 'mod'}}
  end
-	table.insert(bottom_bar, {text = '🎫 اطلاعات, callback_data = fromhelp:info'}) 
+	table.insert(bottom_bar, {text = '🎫 Info', callback_data = 'fromhelp:info'}) --insert the "Info" button
 	table.insert(keyboard.inline_keyboard, bottom_bar)
 	return keyboard
 end
