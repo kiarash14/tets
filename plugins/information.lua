08d52d6268c6aef6acc13a442d19ed520a08305d
Arian <AllwenWaker81@gmail.com>
2016-08-27 13:20:59 +0430

    Update information.lua

--- plugins/information.lua
+++ plugins/information.lua
@@ -11,12 +11,21 @@ local function do_keybaord_credits2()
 	}
 	return keyboard
 end
-if msg.cb then
+local action = function(msg, blocks, ln)
+    if blocks[1] == 'information' or blocks[1] == 'information' then
+        db:hset('bot:users', msg.from.id, 'xx')
+        db:hincrby('bot:general', 'users', 1)
+        if msg.chat.type == 'private' then
+            local message = [[🎲 اطلاعات مفید:]]
+            local keyboard = do_keyboard_credits2()
+            api.sendKeyboard(msg.from.id, message, keyboard, true)
+            end
+    if msg.cb then
         local query = blocks[1]
         local msg_id = msg.message_id
         local text
 if query == 'robot' then
-		local text = '🕔 Bot Creator And Version: `'..config.version..'`\n🔗 '..lang[msg.ln].credits, keyboard
+		local text = [[🎲 اطلاعات مفید:]]
             local keyboard = do_keyboard_credits2()
         api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
 end
@@ -26,6 +35,7 @@ end
 return {
 	action = action,
 	triggers = = { 
+	    '^/(information)$',
 	    '^###cb:!(robot)',
     }
 }
