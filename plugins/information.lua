762d4ca9e1b594d5463b536d2f22f107721b1878
Arian <AllwenWaker81@gmail.com>
2016-08-27 13:32:23 +0430

    Update information.lua

--- plugins/information.lua
+++ plugins/information.lua
@@ -1,24 +1,52 @@
-local function do_keybaord_credits2()
-	local keyboard = {}
+local function do_keyboard_robot()
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+		{
+    					{text = 'Share Robot Contact 🤖🤘🏾', callback_data = '!share'},
+    					},
+    					{
+    		    		{text = 'Buy Group 💸', callback_data = '!buygroup'},
+    		    		{text = 'Support 👥', url = 'https://telegram.me/joinchat/BvytAD9KL7J2PE2u0ek3ZA'},
+	    },
+	    {
+	    {text = '🔙', callback_data = '!home'}
+        }
+    }
+    return keyboard
+end
+
+local function do_keyboard_startme()
+    local keyboard = {}
     keyboard.inline_keyboard = {
     	{
-    		{text = '🎫 کانال ما', url = 'https://telegram.me/spamproofchannel'},
-    		{text = '📣 رای دادن', url = 'https://telegram.me/storebot?start=spamproofbot'},
-		},
-		{
-			{text = '👥 گروه ها', callback_data = 'private:groups'},
-		}
-	}
-	return keyboard
+    		{text = '🙃👉 Click here ! 👈🙃', url = 'https://telegram.me/'..bot.username}
+	    }
+    }
+    return keyboard
 end
+
+local action = function(msg, blocks, ln)
+    if blocks[1] == 'start2' or blocks[1] == 'help2' then
+        db:hset('bot:users', msg.from.id, 'xx')
+        db:hincrby('bot:general', 'users', 1)
+        if msg.chat.type == 'private' then
+            local message = [[📊 اطلاعات :]]
+            local keyboard = do_keyboard_robot()
+            api.sendKeyboard(msg.from.id, message, keyboard, true)
+            end
+			if msg.chat.type == 'group' or msg.chat.type == 'supergroup' then
+          api.sendKeyboard(msg.chat.id, 'Hey 👋 Please `start` me in *PV* 🖐😄👇' ,do_keyboard_startme(), true)
+        end
+        return
+    end
 
     if msg.cb then
         local query = blocks[1]
         local msg_id = msg.message_id
         local text
 if query == 'robot' then
-	    local text = [[🎲 اطلاعات مفید:]]
-            local keyboard = do_keyboard_credits2()
+            local text = [[📊 اطلاعات :]]
+            local keyboard = do_keyboard_robot()
         api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
 end
     end
@@ -27,7 +55,11 @@ end
 
 return {
 	action = action,
-	triggers = { 
+	triggers = {
+	    '^/(start2)@BeatBotTeamBot$',
+	    '^/(start2)$',
+	    '^/(help2)$',
 	    '^###cb:!(robot)',
+
     }
 }
