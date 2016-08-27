66b2995d8eba9e97fa98fe3dffc173320eb58aa0
Arian <AllwenWaker81@gmail.com>
2016-08-27 13:12:26 +0430

    Update information.lua

--- plugins/information.lua
+++ plugins/information.lua
@@ -6,13 +6,13 @@ local function do_keybaord_credits2()
     		{text = '📣 رای دادن', url = 'https://telegram.me/storebot?start='..bot.username},
 		},
 		{
-			{text = '👥 گروه ها', callback_data = 'private:groups'}
+			{text = '👥 گروه ها', callback_data = 'private:groups'},
 		}
 	}
 	return keyboard
 end
 if query == 'robot' then
-		local text = '🕔 Bot Creator And Version: `'..config.version..'`\n🔗 '..lang[msg.ln].credits, keyboard_]]
+		local text = '🕔 Bot Creator And Version: `'..config.version..'`\n🔗 '..lang[msg.ln].credits, keyboard
             local keyboard = do_keyboard_credits2()
         api.editMessageText(msg.chat.id, msg_id, text, keyboard, true)
 end
